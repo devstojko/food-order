00309fc2698950fc78dfd96d9b98ff03bd2462de
Sava <sava.vuckovic@quantox.com>
2019-02-05 13:25:09 +0100

    added image optimization for production

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,11 +1,36 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const ImageminPlugin = require("imagemin-webpack");
+const imageminGifsicle = require("imagemin-gifsicle");
+const imageminJpegtran = require("imagemin-jpegtran");
+const imageminOptipng = require("imagemin-optipng");
+const imageminSvgo = require("imagemin-svgo");
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
   plugins: [
-    new OptimizeCssAssetsPlugin()
+    new OptimizeCssAssetsPlugin(),
+    new ImageminPlugin({
+      bail: false,
+      cache: true,
+      imageminOptions: {
+        plugins: [
+          imageminGifsicle({
+            interlaced: true
+          }),
+          imageminJpegtran({
+            progressive: true
+          }),
+          imageminOptipng({
+            optimizationLevel: 5
+          }),
+          imageminSvgo({
+            removeViewBox: true
+          })
+        ]
+      }
+    })
   ]
 });
