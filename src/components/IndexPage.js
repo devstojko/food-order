8b41302661b8856a5cbb1b1fa48370b07b8307d2
Sava <sava.vuckovic@quantox.com>
2019-02-07 15:00:09 +0100

    added material style to the input field

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// testing
+import InputField from './InputField';
 
-const IndexPage = () => (
-  <div className="homepage">
-    <h1>Index Page Content</h1>
-    <Link to="/signup">Sign Up</Link>
-    <Link to="/signin">Sign In</Link>
-    <Link to="/forgot-password">Forgot Password</Link>
-  </div>
-);
+class IndexPage extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    }
+  }
+
+  render() {
+    return (
+      <div className="homepage">
+        <h1>Index Page Content</h1>
+        <Link to="/signup">Sign Up</Link>
+        <Link to="/signin">Sign In</Link>
+        <Link to="/forgot-password">Forgot Password</Link>
+
+        <hr/>
+        <InputField
+          type="text"
+          name="test"
+          label="test label"
+          value={this.state.value}
+          onChange={e => this.setState({ value: e.target.value })}
+        />
+      </div>
+    )
+  }
+}
+  
+
 
 export default IndexPage;
---
+++ src/components/InputField/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './inputField.scss';
+
+const InputField = ({ type, name, label, value, onChange, required = true }) => (
+  <div className="field">
+    <input
+      className="field__text"
+      type={type}
+      name={name}
+      id={name}
+      value={value}
+      onChange={onChange}
+      required={required}
+    />
+    <span className="field__highlight"></span>
+    <span className="field__bar"></span>
+    <label className="field__label" htmlFor={name}>
+      {label}
+    </label>
+    {/* <span className="field__error">Some error here</span> */}
+  </div>
+);
+
+export default InputField;
