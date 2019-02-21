90333307e02d7ea18087fa21c11b93737ffe51ba
Sava <sava.vuckovic@quantox.com>
2019-02-21 15:41:41 +0100

    finished styling navbar dropdown and sidebar

--- src/components/protected/DashboardPage/Navbar/index.js
+++ src/components/protected/DashboardPage/Navbar/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import userImg from '../../../../images/user.png';
 import './Navbar.scss';
 
 const Navbar = () => (
@@ -8,7 +9,24 @@ const Navbar = () => (
       <i className="fas fa-search" />
       <input type="text" placeholder="Search transactions, invoices or help" />
     </div>
-    <div className="navbar__links">Links....</div>
+    <div className="navbar__links">
+      <div className="navbar__icons">
+        <i className="fas fa-question-circle" />
+        <i className="fas fa-comments" />
+        <i className="fas fa-bell" />
+        <span className="navbar__separator" />
+      </div>
+
+      <div className="navbar__user">
+        User Name
+        <img className="navbar__avatar" src={userImg} alt="user" />
+        <div className="navbar__dropdown">
+          <div className="navbar__dropdown-link">Random Link 1</div>
+          <div className="navbar__dropdown-link">Random Link 2</div>
+          <div className="navbar__dropdown-link">Logout</div>
+        </div>
+      </div>
+    </div>
   </nav>
 );
 
