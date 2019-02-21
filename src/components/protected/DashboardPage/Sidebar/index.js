664dba98735dcdbc63fca199512adc97f14b1ba7
Sava <sava.vuckovic@quantox.com>
2019-02-21 12:58:39 +0100

    rendering sidebar items

--- src/components/protected/DashboardPage/Sidebar/index.js
+++ src/components/protected/DashboardPage/Sidebar/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import SidebarItem from './SidebarItem';
 import './Sidebar.scss';
 
-const Sidebar = () => <aside className="sidebar">Sidebar</aside>;
+const links = [
+  { iconClass: 'fas fa-home', text: 'Rooms' },
+  { iconClass: 'far fa-envelope', text: 'Inbox' },
+  { iconClass: 'fab fa-product-hunt', text: 'Products' },
+  { iconClass: 'fas fa-receipt', text: 'Invoices' },
+  { iconClass: 'far fa-user', text: 'Customers' },
+  { iconClass: 'far fa-comments', text: 'Chat Room' },
+  { iconClass: 'far fa-calendar-alt', text: 'Calendar' },
+  { iconClass: 'fas fa-cog', text: 'Settings' }
+];
+
+const Sidebar = () => (
+  <aside className="sidebar">
+    {links.map((link, i) => (
+      <SidebarItem key={i} link={link} />
+    ))}
+  </aside>
+);
 
 export default Sidebar;
