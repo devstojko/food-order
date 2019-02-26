3f1503a602549756bc7ce81b844fb4e3ef3829c7
Sava <sava.vuckovic@quantox.com>
2019-02-26 16:45:45 +0100

    styled chat list

--- src/components/dashboard/ChatPage/ChatList/index.js
+++ src/components/dashboard/ChatPage/ChatList/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import Avatar from '../../../common/Avatar';
 import './ChatList.scss';
 
-const dummyChats = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+const dummyChats = [1, 2, 3, 4, 5, 6];
+
+const ChatListItem = () => (
+  <div className="chat-item">
+    <Avatar />
+    <div className="chat-item__text">
+      <strong>User Name</strong>
+      <span>Last message text</span>
+    </div>
+    <div className="chat-item__time">13 min ago</div>
+  </div>
+);
 
 const ChatList = () => (
   <div className="chat-list">
     {dummyChats.map(c => (
-      <div key={c} className="chat-list__item">
-        {c}
-      </div>
+      <ChatListItem key={c} />
     ))}
   </div>
 );
