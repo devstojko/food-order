b112a14f0ac78fc3129d9b94ceb3242363e5f2db
Sava <sava.vuckovic@quantox.com>
2019-02-26 13:25:25 +0100

    added link back to not found page

--- src/components/layout/DashboardLayout/Sidebar/index.js
+++ src/components/layout/DashboardLayout/Sidebar/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SidebarItem from './SidebarItem';
+import { NavLink } from 'react-router-dom';
 import './Sidebar.scss';
 
 const links = [
@@ -8,15 +8,18 @@ const links = [
   { iconClass: 'fab fa-product-hunt', text: 'Products' },
   { iconClass: 'fas fa-receipt', text: 'Invoices' },
   { iconClass: 'far fa-user', text: 'Customers' },
-  { iconClass: 'far fa-comments', text: 'Chat Room' },
+  { iconClass: 'far fa-comments', text: 'Chat Room', to: '/chat' },
   { iconClass: 'far fa-calendar-alt', text: 'Calendar' },
-  { iconClass: 'fas fa-cog', text: 'Settings' }
+  { iconClass: 'fas fa-cog', text: 'Settings', to: '/profile-settings' }
 ];
 
 const Sidebar = () => (
   <aside className="sidebar">
     {links.map((link, i) => (
-      <SidebarItem key={i} link={link} />
+      <NavLink key={i} to={link.to || '/404'} className="sidebar__item">
+        <i className={link.iconClass} />
+        {link.text}
+      </NavLink>
     ))}
   </aside>
 );
--- src/components/static/NotFoundPage/index.js
+++ src/components/static/NotFoundPage/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from '../../common/Button';
 import './NotFoundPage.scss';
 
-const NotFoundPage = () => (
+const NotFoundPage = ({ history }) => (
   <div className="not-found">
     <h1 className="title-primary">404 - Page Not Found</h1>
+    <Button text="Go Back" onClick={history.goBack} />
   </div>
 );
 
