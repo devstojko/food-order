9ed900ae8d089bf2c02189860bd271bf4b6e5937
Sava <sava.vuckovic@quantox.com>
2019-03-13 11:28:39 +0100

    fixed sidebar layout

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 TODO:
 
 - add avatar for every user
+- try to reproduce memory leak from chat page and fix it if it happens
 - finish working on the modal for adding group chats
 - OPTIONAL - "User is typing" feature
 - restrict the amount of info fetched for users (don't fetch passwords)
--- src/components/dashboard/ChatPage/chatContext/Provider.js
+++ src/components/dashboard/ChatPage/chatContext/Provider.js
@@ -21,113 +21,113 @@ class Provider extends Component {
     };
   }
 
-  // componentDidMount() {
-  //   const authUserID = this.props.authUser.id;
+  componentDidMount() {
+    const authUserID = this.props.authUser.id;
 
-  //   this.listener = firebase.fetchMyChats(authUserID).onSnapshot(snapshot => {
-  //     this.setState({ myChats: [] });
-  //     snapshot.forEach(doc => {
-  //       const chat = { id: doc.id };
-  //       const data = doc.data();
+    this.listener = firebase.fetchMyChats(authUserID).onSnapshot(snapshot => {
+      this.setState({ myChats: [] });
+      snapshot.forEach(doc => {
+        const chat = { id: doc.id };
+        const data = doc.data();
 
-  //       if (data.groupName) {
-  //         // if chat is group chat, attach group name to chat
-  //         chat.groupName = data.groupName;
-  //         chat.participants = data.participants;
-  //         chat.avatar = data.avatar;
-  //         this.setState({ myChats: [...this.state.myChats, chat] });
-  //       } else {
-  //         // find other user and attach his data to chat
-  //         const otherUserRef =
-  //           data.participants[0].id === authUserID
-  //             ? data.participants[1]
-  //             : data.participants[0];
+        if (data.groupName) {
+          // if chat is group chat, attach group name to chat
+          chat.groupName = data.groupName;
+          chat.participants = data.participants;
+          chat.avatar = data.avatar;
+          this.setState({ myChats: [...this.state.myChats, chat] });
+        } else {
+          // find other user and attach his data to chat
+          const otherUserRef =
+            data.participants[0].id === authUserID
+              ? data.participants[1]
+              : data.participants[0];
 
-  //         otherUserRef.get().then(user => {
-  //           chat.otherUser = {
-  //             id: user.id,
-  //             ...user.data()
-  //           };
+          otherUserRef.get().then(user => {
+            chat.otherUser = {
+              id: user.id,
+              ...user.data()
+            };
 
-  //           this.setState({ myChats: [...this.state.myChats, chat] });
-  //         });
-  //       }
-  //     });
-  //   });
-  // }
+            this.setState({ myChats: [...this.state.myChats, chat] });
+          });
+        }
+      });
+    });
+  }
 
-  // handleSearchChange(e) {
-  //   this.setState({ searchTerm: e.target.value }, () => {
-  //     this.getUsers(this.state.searchTerm);
-  //   });
-  // }
+  handleSearchChange(e) {
+    this.setState({ searchTerm: e.target.value }, () => {
+      this.getUsers(this.state.searchTerm);
+    });
+  }
 
-  // getUsers(term) {
-  //   this.setState({ users: [] });
-  //   firebase
-  //     .fetchUsersByName(term.toLowerCase())
-  //     .then(snapshots => {
-  //       if (!snapshots.empty) {
-  //         snapshots.forEach(u => {
-  //           const privateChats = this.state.myChats.filter(c => !c.groupName);
-  //           // add user to search results only if we don't have a private chat
-  //           if (!privateChats.find(c => c.otherUser.id === u.id)) {
-  //             const user = { id: u.id, ...u.data() };
-  //             this.setState({ users: [...this.state.users, user] });
-  //           }
-  //         });
-  //       }
-  //     })
-  //     .catch(err => console.log(err));
-  // }
+  getUsers(term) {
+    this.setState({ users: [] });
+    firebase
+      .fetchUsersByName(term.toLowerCase())
+      .then(snapshots => {
+        if (!snapshots.empty) {
+          snapshots.forEach(u => {
+            const privateChats = this.state.myChats.filter(c => !c.groupName);
+            // add user to search results only if we don't have a private chat
+            if (!privateChats.find(c => c.otherUser.id === u.id)) {
+              const user = { id: u.id, ...u.data() };
+              this.setState({ users: [...this.state.users, user] });
+            }
+          });
+        }
+      })
+      .catch(err => console.log(err));
+  }
 
-  // setOtherUser(user) {
-  //   this.setState({
-  //     otherUser: user,
-  //     activeChat: null
-  //   });
-  // }
+  setOtherUser(user) {
+    this.setState({
+      otherUser: user,
+      activeChat: null
+    });
+  }
 
-  // setActiveChat(chat) {
-  //   this.setState({
-  //     activeChat: chat,
-  //     otherUser: null
-  //   });
-  // }
+  setActiveChat(chat) {
+    this.setState({
+      activeChat: chat,
+      otherUser: null
+    });
+  }
 
-  // handleChatCreate() {
-  //   this.setState({
-  //     searchTerm: '',
-  //     otherUser: null
-  //   });
-  // }
+  handleChatCreate() {
+    this.setState({
+      searchTerm: '',
+      otherUser: null
+    });
+  }
 
-  // toggleModal() {
-  //   this.setState({ showModal: !this.state.showModal });
-  // }
+  toggleModal() {
+    this.setState({ showModal: !this.state.showModal });
+  }
 
-  // componentWillUnmount() {
-  //   this.listener();
-  // }
+  componentWillUnmount() {
+    this.listener();
+  }
 
   render() {
     const contextValue = {
-      ...this.state
-      // getUsers: debounce(this.getUsers.bind(this), 300),
-      // setOtherUser: this.setOtherUser.bind(this),
-      // setActiveChat: this.setActiveChat.bind(this),
-      // handleSearchChange: this.handleSearchChange.bind(this),
-      // handleChatCreate: this.handleChatCreate.bind(this),
-      // toggleModal: this.toggleModal.bind(this)
+      ...this.state,
+      getUsers: debounce(this.getUsers.bind(this), 300),
+      setOtherUser: this.setOtherUser.bind(this),
+      setActiveChat: this.setActiveChat.bind(this),
+      handleSearchChange: this.handleSearchChange.bind(this),
+      handleChatCreate: this.handleChatCreate.bind(this),
+      toggleModal: this.toggleModal.bind(this)
     };
 
     return (
       <ChatContext.Provider value={contextValue}>
         {this.props.children}
 
-        {/* {this.state.showModal && (
+        {this.state.showModal && (
           <GroupChatModal handleClose={this.toggleModal.bind(this)} />
-        )} */}
+        )}
       </ChatContext.Provider>
     );
   }
