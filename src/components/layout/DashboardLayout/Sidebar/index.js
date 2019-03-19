b127eff19835be28d289b8a323314f925d164c66
Sava <sava.vuckovic@quantox.com>
2019-03-19 12:38:18 +0100

    sidebar responsive for tablet screens

--- src/components/layout/DashboardLayout/Sidebar/index.js
+++ src/components/layout/DashboardLayout/Sidebar/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import './Sidebar.scss';
@@ -14,17 +15,36 @@ const links = [
   { iconClass: 'fas fa-cog', text: 'Settings', to: '/profile-settings' }
 ];
 
+const SidebarLinks = ({ showText }) =>
+  links.map((link, i) => (
+    <NavLink key={i} to={link.to || '/404'} className="sidebar__item">
+      <i className={link.iconClass} />
+      {showText && link.text}
+    </NavLink>
+  ));
+
+SidebarLinks.propTypes = {
+  showText: PropTypes.bool
+};
+
 const Sidebar = () => (
   <aside className="sidebar">
     <div className="sidebar__logo">
-      <Link to="/">Food-Order</Link>
+      <Link to="/" className="link-normal">
+        Food-Order
+      </Link>
+
+      <Link to="/" className="link-tablet">
+        F
+      </Link>
     </div>
-    {links.map((link, i) => (
-      <NavLink key={i} to={link.to || '/404'} className="sidebar__item">
-        <i className={link.iconClass} />
-        {link.text}
-      </NavLink>
-    ))}
+
+    <div className="sidebar__links-normal">
+      <SidebarLinks showText={true} />
+    </div>
+    <div className="sidebar__links-tablet">
+      <SidebarLinks showText={false} />
+    </div>
   </aside>
 );
 
