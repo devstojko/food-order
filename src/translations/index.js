d24ee6346fb3a6f0c1e67062c9ffa55c176a79fc
Sava <sava.vuckovic@quantox.com>
2019-03-21 12:25:32 +0100

    added english text to translations

--- src/translations/index.js
+++ src/translations/index.js
@@ -1,6 +1,59 @@
 export default {
   en: {
-    welcome: 'Welcome'
+    // auth
+    welcomeBack: 'Welcome back! Please login to your account.',
+    createAcc: 'Please complete to create your account',
+    firstName: 'First Name',
+    lastName: 'Last Name',
+    username: 'Username',
+    email: 'Email',
+    password: 'Password',
+    confirmPassword: 'Confirm Password',
+    signup: 'Sign Up',
+    login: 'Login',
+    googleLogin: 'Sign in with Google',
+    forgotPw: 'Forgot Password',
+    rememberMe: 'RememberMe',
+    alreadyRegistered: 'Already have an account? Sign in.',
+    notRegistered: "Don't have an account? Sign up.",
+    pwResetEmail: 'Enter your email and we send you a password reset link',
+    sendEmail: 'Send request',
+    terms: 'Terms of use. Privacy policy',
+    // dashboard sidebar
+    rooms: 'Rooms',
+    inbox: 'Inbox',
+    products: 'Products',
+    invoices: 'Invoices',
+    customers: 'Customers',
+    chat: 'Chat Room',
+    calendar: 'Calendar',
+    settings: 'Settings',
+    search: 'Search transactions, invoices or help',
+    // home page
+    welcome: 'Welcome',
+    active: 'active',
+    ordering: 'ordering',
+    isOrdering: 'is ordering food',
+    // settings page
+    changeInfo: 'Change your info',
+    changePw: 'Change password',
+    updateProfile: 'Update your profile',
+    update: 'Update',
+    updatePw: 'Update your password',
+    resetPw: 'Reset Password',
+    // chat page
+    groupChats: 'Group Conversations',
+    privateChats: 'Private Conversations',
+    startChat: 'Start a conversation by sending a message',
+    createGroupChat: 'Create Group Chat',
+    enterGroupName: 'Enter Group Name',
+    addUsers: 'Add users to this conversation',
+    goBack: 'Go Back',
+    addChatParticipants: 'Add Conversation Participants',
+    searchResults: 'Search Results',
+    you: 'you',
+    participants: 'participants',
+    typeMessage: 'Type your message here...'
   },
   sr: {
     welcome: 'Dobrodosli'
